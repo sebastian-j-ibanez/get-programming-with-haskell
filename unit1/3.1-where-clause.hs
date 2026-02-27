b9ac382b664866bc0e028e370fb7b567f43ced40
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2026-02-26 23:47:32 -0500

    feat: add lambda sumSquareOrSquareSum to 3.1

--- unit1/3.1-where-clause.hs
+++ unit1/3.1-where-clause.hs
@@ -1,6 +1,15 @@
--- Writing my own where clause.
+-- Writing our own where clause.
 
 sumSquareOrSquareSum x y = max sumSquare squareSum
   where
     sumSquare = x ^ 2 + y ^ 2
     squareSum = (x + y) ^ 2
+
+lambdaSumSquareOrSquareSum x y =
+  ( \sumSquare squareSum ->
+      if sumSquare > squareSum
+        then sumSquare
+        else squareSum
+  )
+    (x ^ 2 + y ^ 2)
+    ((x + y) ^ 2)
